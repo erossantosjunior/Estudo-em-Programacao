e21a695ceec4ec4750460af7e3e26e3f93248603
Eros Santos Junior <eros.santos.jr@hotmail.com>
2025-02-19 13:29:43 -0300

    Aplicando o from e import

--- Python/sabor-express2.0/modelos/restaurante.py
+++ Python/sabor-express2.0/modelos/restaurante.py
@@ -2,23 +2,23 @@ class Restaurante:
     restaurantes = []
 
     def __init__(self, nome, categoria):
-        self.nome = nome
-        self.categoria = categoria
+        self._nome = nome.title()
+        self.categoria = categoria.upper()
         self._ativo = False
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
-        return f'{self.nome} | {self.categoria}'
+        return f'{self._nome} | {self.categoria}'
     
-    def listar_restaurantes():
-        for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}')
+    @classmethod
+    def listar_restaurantes(cls):
+        for restaurante in cls.restaurantes:
+            print(f'{restaurante._nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}')
 
     @property
     def ativo(self):
         return '☑' if self._ativo else '☐'
 
-restaurante_praca = Restaurante('Praça', 'Gourmet')
-restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
+    def alternar_estado(self):
+        self._ativo = not self._ativo
 
-Restaurante.listar_restaurantes()
