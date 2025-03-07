50ec7619039a22e020532e666ed28cd1ce82bb6a
erossantosjunior <eros.santos.jr@hotmail.com>
2025-03-07 09:13:43 -0300

    Atualização de código de estudo

--- Python/sabor-express2.0/app.py
+++ Python/sabor-express2.0/app.py
@@ -5,11 +5,11 @@ from modelos.cardapio.prato import Prato
 restaurante_praca = Restaurante('praça', 'Gourmet')
 bebida_suco = Bebida('Suco de Melancia', 5.0, 'Grande')
 prato_paozinho = Prato('Paozinho', 2.00, 'O melhor pão da cidade')
-a
+restaurante_praca.adicionar_no_cardapio(bebida_suco)
+restaurante_praca.adicionar_no_cardapio(prato_paozinho)
 
 def main():
-    print(bebida_suco)
-    print(prato_paozinho)
+    restaurante_praca.exibir_Cardapio
 
 if __name__ == '__main__':
     main()
--- Python/sabor-express2.0/modelos/restaurante.py
+++ Python/sabor-express2.0/modelos/restaurante.py
@@ -1,4 +1,5 @@
 from modelos.avaliacao import Avaliacao
+from modelos.cardapio.item_cardapio import ItemCardapio
 
 class Restaurante:
     restaurantes = []
@@ -8,6 +9,7 @@ class Restaurante:
         self.categoria = categoria.upper()
         self._ativo = False
         self._avaliacao = []
+        self._cardapio = []
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
@@ -39,3 +41,21 @@ class Restaurante:
         quantidade_de_notas = len(self._avaliacao)
         media = round(soma_das_notas / quantidade_de_notas, 1)
         return media
+    
+    #def adicionar_bebida_no_cardapio(self,bebida):
+    #    self._cardapio.append(bebida)
+
+
+    #def adicionar_prato_no_cardapio(self,prato):
+    #    self._cardapio.append(prato)
+
+    def adicionar_no_cardapio(self,item):
+        if isinstance(item,ItemCardapio):
+            self._cardapio.append(item)
+
+    @property
+    def exibir_Cardapio(self):
+        print(f'Cardapio do restaurante {self._nome}\n')
+        for i, item in enumerate(self._cardapio, start=1):
+            mensagem = f'{i}. Nome:{item._nome} | Preço: R${item._preco}'
+            print(mensagem)
